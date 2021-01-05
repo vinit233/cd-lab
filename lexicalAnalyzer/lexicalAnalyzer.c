9a808c6dbe45f7edeca27e1c4f5e3b1a83efe32e
vinit233 <76988837+vinit233@users.noreply.github.com>
2021-01-05 11:38:01 +0530

    lab4

---
+++ lexicalAnalyzer/lexicalAnalyzer.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+int main()
+{
+    char i, str[50];
+    int opr = 0, var = 0, con = 0;
+    printf("Name: vinit chauhan \nRoll No: 1816110242 \n");
+    printf("Enter expression:");
+    scanf(" %s", str);
+    char *ptr = str;
+    while (*ptr != '\0')
+    {
+        if (isalpha(*ptr))
+        {
+            var++;
+            while (isalnum(*ptr))
+                ptr++;
+        }
+        else if (!isalnum(*ptr))
+        {
+            opr++;
+            while (!isalnum(*ptr))
+                ptr++;
+        }
+        else if (isdigit(*ptr))
+        {
+            con++;
+            while (isdigit(*ptr))
+                ptr++;
+        }
+    }
+    printf("variable=%d\n", var);
+    printf("operator=%d\n", opr);
+    printf("constant=%d\n", con);
+    printf("tokens=%d", var + opr + con);
+    return 0;
+}
